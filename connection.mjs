84b72ed6a6dbc766fe8fef3df8b64d2062b01013
Bautista Sobenko <bautipriano@gmail.com>
2023-11-27 22:23:56 -0300

    Conexion con AWS rds

---
+++ connection.mjs
@@ -0,0 +1,27 @@
+import pgClient from 'pg';
+import dotenv from 'dotenv';
+
+const {Client} = pgClient;
+dotenv.config();
+
+(async () => {
+
+  const client = new Client({
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+  }
+  });
+
+  await client.connect();
+
+  const res = await client.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
+  console.log(res.rows[0].connected);
+
+  await client.end();
+})();
+
