6f196cff6a530cdd5d9b0c20d159465f626f1357
Bautista Sobenko <bautipriano@gmail.com>
2023-11-29 20:46:51 -0300

    fixes

--- connection.mjs
+++ connection.mjs
@@ -1,85 +1,44 @@
-import pgClient from 'pg';
-import dotenv from 'dotenv';
-
-const {Client} = pgClient;
-dotenv.config();
+import { Empleado } from './crudEmpleados.mjs';
 
 (async () => {
-
-  const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+  
+  const empleado = new Empleado();
 
   try {
 
-    await client.connect();
+    await empleado.conectarBD();
 
-    client.query("BEGIN");
+    const nuevoEmpleado = {
+        nombre: 'John Quilis',
+        tipo_doc: 'dni',
+        nro_doc: '15453135',
+        fecha_nacimiento: '2000-01-01',
+        domicilio: 'Calle Principal 123',
+        categoria: 'noProfesional',
+        password: 'contraseña123',
+        huella_dactilar: '11110001111101010111011010111101110011',
+    };
 
-    let result = await client.query("CALL obtener_empleados_autorizados()");
-    console.log("EMPLEADOS AUTORIZADOS: " + result.rows);
-
-    result = await client.query("CALL obtener_empleados_con_intentos_fallidos()");
-    console.log("EMPLEADOS CON INTENTOS FALLIDOS: " + result.rows);
-
-    const query = 'SELECT * FROM vista_intentos_fallidos;';
-    result = await pool.query(query);
-
-    // SITUACION DE COMMIT
+    //const empleadoCreado = await empleado.agregarEmpleado(nuevoEmpleado);
+    //console.log('Empleado creado:', empleadoCreado);
+    
+    //const empleadoEliminado = await empleado.eliminarEmpleado(513);
+    //console.log('Empleado eliminado:', empleadoEliminado);
 
     const modificables = {
       nombre: null,
-      domicilio: "Lamadrid 2271",
-      categoria: null,
+      domicilio: null,
       password: null,
     }
 
-    actualizarEmpleado(182, modificables);
-
-    eliminarEmpleado(2);
-
-
-    const nuevoEmpleado = {
-        nombre: 'John Doe',
-        tipo_doc: 'dni',
-        nro_doc: '12345678',
-        fecha_nacimiento: '2000-01-01',
-        domicilio: 'Calle Principal 123',
-        categoría: 'no_profesional',
-        password: 'contraseña123',
-        huella_dactilar: '111100011111010101011001',
-    };
-
-    const empleadoCreado = await crearEmpleado(nuevoEmpleado);
-    console.log('Empleado creado:', empleadoCreado);
-
-    // Actualizar el nombre de un empleado
-    const empleadoActualizado = await actualizarEmpleado(123, 'John Smith');
-    console.log('Empleado actualizado:', empleadoActualizado);
-
-    // Leer todos los empleados
-    const empleados = await leerEmpleados();
-    console.log('Lista de empleados:', empleados);
-
-    // Eliminar un empleado
-    const empleadoEliminado = await eliminarEmpleado(123);
-    console.log('Empleado eliminado:', empleadoEliminado);
-
-    client.query("COMMIT");
+    //const empleadoModificado = await empleado.actualizarEmpleado(500, modificables);
+    //console.log('Empleado modificado:', empleadoModificado);
 
   } catch (error) {
     console.error('Error en la aplicación:', error.message);
-    client.query("ROLLBACK");
 
   } finally {
-    await client.end();
+    await empleado.cerrarConexion();
   }
 }) ();
   
---
+++ crudEmpleados.mjs
@@ -0,0 +1,145 @@
+import pgClient from 'pg';
+import dotenv from 'dotenv';
+
+const {Client} = pgClient;
+dotenv.config();
+
+class Empleado{
+
+    constructor(){
+        this.client = new Client({
+          host: process.env.PG_HOST,
+          port: process.env.PG_PORT,
+          user: process.env.PG_USER,
+          password: process.env.PG_PASSWORD,
+          database: process.env.PG_DATABASE,
+          ssl: {
+            rejectUnauthorized: false
+          }
+      });
+    }
+
+    async conectarBD(){
+        try{
+            await this.client.connect();
+            console.log('Conectado con exito a la base de datos')
+        }
+        catch(e){
+            console.log(e);
+            console.log('Error en la conexion a la base de datos')
+        }
+    }
+
+
+    async agregarEmpleado(empleado){
+
+        const query = `
+        INSERT INTO empleado (nombre, tipo_doc, nro_doc, fecha_nacimiento, domicilio, categoria, password, huella_dactilar)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING *;
+        `;
+
+        const values = [
+            empleado.nombre,
+            empleado.tipo_doc,
+            empleado.nro_doc,
+            empleado.fecha_nacimiento,
+            empleado.domicilio,
+            empleado.categoria,
+            empleado.password,
+            empleado.huella_dactilar,
+        ];
+
+        try{
+            return (await this.client.query(query, values)).rows;
+        }
+        catch(error){
+            console.log(error);
+            console.log('no se pudo agregar el empleado')
+        }
+
+    }
+
+    async actualizarEmpleado(nro_identificacion, modificables) {
+      try {
+      
+        let query = "UPDATE empleado SET";
+    
+        const claves = Object.keys(modificables);
+
+        claves.forEach( key => {
+            if( modificables[key] !== null ) {
+              query += ` ${key} = '${modificables[key]}'`;
+            }
+        })
+
+        
+        query += "WHERE nro_identificacion = $1 RETURNING *;";
+        
+        console.log(query);
+
+        const values = [nro_identificacion];
+    
+        const result = await this.client.query(query, values);
+        return result.rows[0];
+    
+      } catch (error) {
+        console.error('Error al actualizar el empleado:', error.message);
+        throw error;
+      }
+    };
+
+    async mostrarEmpleados(){
+        const query = `
+        SELECT * FROM empleado "
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+        RETURNING *;
+        `;
+
+        try{
+            return (await client.query(query, values)).rows;
+        }
+        catch(error){
+            console.log('no se pudieron mostrar los empleados')
+        }
+    }
+
+    async leerEmpleados() {
+        try {
+          const query = 'SELECT * FROM empleado;';
+          const result = await client.query(query);
+          return result.rows;
+        } catch (error) {
+          console.error('Error al leer los empleados:', error.message);
+          throw error;
+        }
+      };
+      
+      async eliminarEmpleado(nro_identificacion) {
+        try {
+          const query = `
+            DELETE FROM empleado
+            WHERE nro_identificacion = $1
+            RETURNING *;
+          `;
+      
+          const values = [nro_identificacion];
+      
+          const result = await this.client.query(query, values);
+          return result.rows[0];
+        } catch (error) {
+          console.error('Error al eliminar el empleado:', error.message);
+          throw error;
+        }
+      };
+
+      async cerrarConexion() {
+        this.client.end();
+      }
+    
+}
+
+export {
+    Empleado
+}
+
